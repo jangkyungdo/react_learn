768dd3fef192d2844a8b73dcb8cf8337406ada82
jangkyungdo <rudeh0926@naver.com>
2020-07-08 20:14:18 +0900

    스타일링 Sass, styled-component

--- sass/src/App.js
+++ sass/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import SassComponent from "./Components/SassComponent";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div className="App">
-      <SassComponent />
+      <StyledComponent />
     </div>
   );
 }
---
+++ sass/src/StyledComponent.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Box = styled.div`
+  background: ${(props) => props.color || "blue"};
+  padding: 1rem;
+  display: flex;
+  width: 1024px;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Button = styled.button`
+  background: white;
+  color: black;
+  border-radius: 4px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  font-size: 1rem;
+  font-weight: 600;
+
+  &:hover {
+    /* Sass처럼 자기 자신 선택 가능하다. */
+    background: rgba(255, 255, 255, 0.9);
+  }
+  ${(props) =>
+    props.inverted &&
+    css`
+      background: none;
+      border: 2px solid white;
+      color: white;
+      /* css를 표기하지 않으면 문자열로 표기가 되고 Tagged 템플릿이 아니기 때문에 props값을 사용하지 못한다. */
+      &:hover {
+        background: white;
+        color: black;
+      }
+    `};
+  & + button {
+    margin-left: 1rem;
+  }
+`;
+
+const StyledComponent = () => {
+  return (
+    <Box color="black">
+      <Button>안녕하세요</Button>
+      <Button inverted={true}>테두리만</Button>
+    </Box>
+  );
+};
+
+export default StyledComponent;
