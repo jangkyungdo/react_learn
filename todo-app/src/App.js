102216c9cfc47691ce343dc3b43573ce22cf4fbf
jangkyungdo <rudeh0926@naver.com>
2020-07-13 14:11:20 +0900

    성능 최적화

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,59 +1,71 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트 기초 공부하기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: 'todolist 만들기',
+function createBlukTodos() {
+  const array = [];
+  for (let i = 0; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
 
-  const nextId = useRef(4);
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      // type: 'INSET', todo:{ id:1, text: 'todo', checked: false}
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+const App = () => {
+  // useReducer를 사용하여 성능 최적화
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBlukTodos);
+  //컴포넌트가 맨 처음 렌더링될 때만 createBlukTodos 함수가 호출되도록 세번째 파라미터에 초기상태
+  // const [todos, setTodos] = useState(createBlukTodos);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      console.log(todos);
-      console.log(todo);
-      setTodos(todos.concat(todo));
-      console.log(todos);
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    console.log(todos);
+    console.log(todo);
+    dispatch({ type: 'INSERT', todo });
+    // setTodos((todos) => todos.concat(todo));
+    console.log(todos);
+    nextId.current += 1;
+  }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  });
+    dispatch({ type: 'REMOVE', id });
+    // setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({ type: 'TOGGLE', id });
+
+    // setTodos((todos) =>
+    //   todos.map((todo) =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+    //   ),
+    // );
+  }, []);
 
   return (
     <TodoTemplate>
