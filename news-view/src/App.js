f63a33faa5284117cc5a1c884a4e4e249c998f1a
jangkyungdo <rudeh0926@naver.com>
2020-07-21 21:36:38 +0900

    비동기 작업

---
+++ news-view/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=457f9df6b4874be9a69654e62350c128',
+      );
+
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        ></textarea>
+      )}
+    </div>
+  );
+};
+
+export default App;
